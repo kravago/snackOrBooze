c42408af923e3fb83b0ebc92d4f87e7ff1f00c10
Engines-screaming <karl.ravago@gmail.com>
2022-10-08 19:53:38 -0700

    smoke tests pass

--- snack-or-booze/src/FoodItem.test.js
+++ snack-or-booze/src/FoodItem.test.js
@@ -1,12 +1,21 @@
 import { MemoryRouter } from 'react-router-dom';
 import { render } from "@testing-library/react";
 import FoodItem from "./FoodItem";
+import { drinks, snacks } from '../db.json';
 
 describe("Test FoodItem component", () => {
-    it("smoke test", () => {
+    it("smoke test for snacks", () => {
         render(
             <MemoryRouter>
-                <FoodItem />
+                <FoodItem items={snacks} cantFind="/snacks"/>
+            </MemoryRouter>
+        );
+    });
+
+    it("smoke test for drinks", () => {
+        render(
+            <MemoryRouter>
+                <FoodItem items={drinks} cantFind="/drinks"/>
             </MemoryRouter>
         );
     });
--- snack-or-booze/src/FoodMenu.test.js
+++ snack-or-booze/src/FoodMenu.test.js
@@ -1,12 +1,23 @@
 import { MemoryRouter } from 'react-router-dom';
 import { render } from "@testing-library/react";
 import FoodMenu from "./FoodMenu";
+import SnackOrBoozeApi from './Api';
+import { drinks } from '../db.json';
 
 describe("Test FoodMenu component", () => {
-    it("smoke test", () => {
+
+    it("smoke test for drinks", () => {
         render(
             <MemoryRouter>
-                <FoodMenu />
+                <FoodMenu items={ drinks } url="drinks" title="Drinks" />
+            </MemoryRouter>
+        );
+    });
+
+    it("smoke test for snacks", () => {
+        render(
+            <MemoryRouter>
+                <FoodMenu items={ snacks } url="snacks" title="Snacks" />
             </MemoryRouter>
         );
     });
